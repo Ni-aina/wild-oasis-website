636cad04192266b49b4309c4ec6bb1ae6b76898d
Ni-aina <niaina.dev.soft@gmail.com>
2025-01-12 15:47:50 +0300

    OAuth2

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,5 @@
+import DateSelector from "@/app/_component/DateSelector";
+import ReservationForm from "@/app/_component/ReservationForm";
 import TextExpander from "@/app/_component/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
@@ -72,9 +74,13 @@ export default async function Page({ params }) {
       </div>
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+        <h2 className="text-5xl font-semibold text-center mb-10">
+          Reserve {name} today. Pay on arrival.
         </h2>
+        <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]" >
+          <DateSelector />
+          <ReservationForm />
+        </div>
       </div>
     </div>
   );
